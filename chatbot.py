980bd5b9b795b98d6c10d375dd40d2aefeccc68b
Muhammed Shibili <muhammedshibili688@gmail.com>
2025-12-24 19:05:55 +0530

    Éncapsulation, Getter, Setter added

--- chatbot.py
+++ chatbot.py
@@ -1,19 +1,25 @@
 class Chatbot:
-    users = {}
-    def __init__(self, name = "Travis Doe", 
+
+    __user_id = 1
+
+    def __init__(self, name = "", email = "",
                  password = "", loggedin = False):
         
+        self.id = Chatbot.__user_id
+        Chatbot.__user_id += 1
         self.name = name
+        self.email = email
         self.password = password
         self.loggedin = loggedin
 
     def menu(self):
-        print(
-            "Welcome to the Chatbot System"
-            "\n1. Register"
-            "\n2. Login"
-            "\n3. Chat"
-            "\n4. Exit"
+
+        print("""Welcome to the Chatbot System
+            \n1. Register
+            \n2. Login
+            \n3. Chat
+            \n4. Exit
+            \n->"""
             )
         choice = input("Please enter your choice (1-4): ")
         if choice == "1":
@@ -21,55 +27,98 @@ class Chatbot:
         elif choice == "2":
             self.login()
         elif choice == "3":
-            self.chat
+            self.chat()
         elif choice == "4":
             print("Exiting the system. Goodbye!")
             exit()
         else:
             print("Invalid choice. Please try again.")
 
+    @staticmethod       
+    def get_id():
+        return Chatbot.__user_id
+    
+    @staticmethod
+    def set_id(val):
+        Chatbot.__user_id = val
 
+    def get_name(self):
+        return self.name
+    
+    def set_name(self, name):
+        self.name = name
+    
     def register(self):
+
         print("Register a new account")
-        name = input ("Enter your name: ")
-        password = input("Enter your password: ")
-        self.users[name] = password
-        print("Registration Succesful!")
+        self.name = input("Enter your name: ")
+        self.email = input ("Enter your email: ")
+        self.password = input("Enter your password: ")
+        print("Registration Successful !!")
+
+        print("\n")
+        self.menu()
 
     def login(self):
-        print("Login to your account")
-        name = input("Enter your name: ")
-        password = input("Enter your password: ")
 
-        if name in self.users and self.users[name] == password:
-            self.loggedin = True
-            print("Login Successful!\n Welcome, " +name+"!")
-            return True
+        print("Login to your account")
+        email_uname = input("Enter your email/username: ")
+        pwd = input("Enter your password: ")
+
+        if email_uname == '' and pwd == '':
+            print("Please signup first by pressing 1 in the main menu")
+        
         else:
-            print("Login Failed! Please check your credentials.")
-            return False
+            if  (email_uname == self.email or email_uname == self.name) and self.password == pwd:
+                self.loggedin = True
+                print("Login Successful!\nWelcome, " +self.name+" !!")
+                chat_opt = input("\nTo use Chatbot, Type 'chat': ")
+
+                if chat_opt.strip().lower() == 'chat':
+                    self.chat()
+                else:
+                    print("\nSpelling miskate in input...")
+            
+            else:
+                print("Login Failed! Please Enter your correct credentials.")
+                return False
+        
+        print("\n")
+        self.menu()
         
     
     def chat(self):
-        if not self.loggedin:
+
+        if self.loggedin is False:
             print("You must be loggedin first.")
-            return
+            return self.menu()
 
         print("You are now chatting with the bot. Type 'exit' to logout.")
+
         while True:
             user_input = input("You: ")
             if user_input.lower() == 'exit':
                 self.loggedin = False
                 print("You have logged out.")
                 break
+
             else:
                 print("Bot: I'm here to help you!")
+                print("\n")
+        
+        print("\n")
+        self.menu()
+                
+
     def exit(self):
         print("Exiting the system. Goodbye!")
-        
+        exit()
+
+'''       
 if __name__ == "__main__":
     chatbot = Chatbot()
     while True:
         chatbot.menu()
         if chatbot.loggedin:
             chatbot.chat()
+'''
---
+++ trial.py
@@ -0,0 +1,4 @@
+from chatbot import Chatbot
+
+Hari = Chatbot()
+Hari.menu()
